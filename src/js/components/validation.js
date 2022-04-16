6a5badbe070576c675c15cfa40dc88762e6f3a35
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-04-17 00:27:57 +0300

    some fix

--- src/js/components/validation.js
+++ src/js/components/validation.js
@@ -1,22 +1,21 @@
 import JustValidate from 'just-validate';
 import Inputmask from "inputmask";
 
-const validateFormEl = document.querySelector("#validate-form");
-
-if(validateFormEl) {
-  const validation = new JustValidate(
-    '#validate-form', {
-      errorFieldCssClass: 'is-invalid',
-      errorLabelStyle: {
-        color: '#F8483A',
-      },
-    }
-  );
+const initValidation = form => {
+  const validation = new JustValidate(form, {
+    errorFieldCssClass: 'is-invalid',
+    errorLabelStyle: {
+      color: '#F8483A',
+    },
+  });
 
   validation
-    .addField('#validate-input', [{
+    .addField('.validate-input', [{
       rule: 'required',
       errorMessage: 'это поле должно быть заполнено'
     }]);
-
 }
+
+const validateForms = document.querySelectorAll(".validate-form");
+
+validateForms?.forEach(form => initValidation(form))
