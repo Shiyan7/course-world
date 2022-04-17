b9b01958350f26144950af4186b1916339dde34a
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-04-18 00:48:36 +0300

    page contacts complete

--- src/js/components/validation.js
+++ src/js/components/validation.js
@@ -1,19 +1,54 @@
 import JustValidate from 'just-validate';
-import Inputmask from "inputmask";
 
 const initValidation = form => {
+
+  const defaultErrorMessage = 'это поле должно быть заполнено';
+  const contactFormEl = document.querySelector('.contacts-form')
+
   const validation = new JustValidate(form, {
     errorFieldCssClass: 'is-invalid',
     errorLabelStyle: {
       color: '#F8483A',
-    },
+    }
   });
 
   validation
     .addField('.validate-input', [{
       rule: 'required',
-      errorMessage: 'это поле должно быть заполнено'
+      errorMessage: defaultErrorMessage
     }]);
+
+  if(contactFormEl) {
+    const contactFormValidation = new JustValidate(contactFormEl, {
+      errorFieldCssClass: 'is-invalid',
+      errorLabelStyle: {
+        color: '#F8483A',
+      },
+    })
+
+    contactFormValidation
+      .addField('.validate-input', [{
+        rule: 'required',
+        errorMessage: defaultErrorMessage,
+      }])
+      .addField('.validate-email', [
+        {
+          rule: 'required',
+          errorMessage: defaultErrorMessage
+        },
+        {
+          rule: 'email',
+          errorMessage: 'введите коректный email',
+        }
+      ])
+      .addField('.validate-message', [{
+        rule: 'required',
+        errorMessage: defaultErrorMessage,
+      }])
+      .onSuccess((ev) => {
+        contactFormEl?.submit()
+      });
+  }
 }
 
 const validateForms = document.querySelectorAll(".validate-form");
